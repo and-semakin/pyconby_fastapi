9da6db8975319478d9b74e0984a398a9e5f28be6
Andrey Semakin <andrey.semakin@incountry.com>
2020-02-21 13:30:25 +0300

    Version 8 (not full)

--- app/crud.py
+++ app/crud.py
@@ -1,13 +1,15 @@
+from typing import Optional
+
 from sqlalchemy.orm import Session
 
-from . import models, schemas
+from . import models, schemas, security
 
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
 
-def get_user_by_email(db: Session, email: str):
+def get_user_by_email(db: Session, email: str) -> Optional[models.User]:
     return db.query(models.User).filter(models.User.email == email).first()
 
 
@@ -15,13 +17,22 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
 
-def create_user(db: Session, user: schemas.UserCreate):
-    hashed_password = f"insecurefakehashed-{user.password}"
+def create_user(db: Session, user: schemas.UserCreate) -> models.User:
+    hashed_password = security.get_password_hash(user.password)
     db_user = models.User(email=user.email, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
+def authenticate_user(db: Session, email: str, password: str) -> Optional[models.User]:
+    user = get_user_by_email(db, email)
+    if not user:
+        return None
+    if not security.verify_password(password, user.hashed_password):
+        return None
+    return user
 
 
 def get_items(db: Session, skip: int = 0, limit: int = 100):
